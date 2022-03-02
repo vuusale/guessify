1ac3387c6831c23569b35db025f469d9b4df91bf
Khayal <xeyal.ferzelibeyli@gmail.com>
2022-03-03 00:03:02 +0400

    Flask server

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import scraper
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	if request.method == 'POST':
+		url = request.form.get('url')
+		scraper_result = scraper.scraper(url) #Calling scraper function
+		return render_template('index.html', result='Result')
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
